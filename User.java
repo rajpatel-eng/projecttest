8d9a2a3799df4685b761b96c6cd1bbaca2e4d2c2
RAJ PATEL <rajpatel230106@gmail.com>
2026-06-12 13:38:49 +0530

    Update User.java

--- User.java
+++ User.java
@@ -6,7 +6,6 @@ public class User {
     private String name;
     private Integer age;
     private String email;
-
     public User(Long id, String name, Integer age, String email) {
         id = id;
         name = name;
