bce3d83f84eea7de865cb84b83ef6f49d1d3bc35
RAJ PATEL <rajpatel230106@gmail.com>
2026-06-12 09:43:28 +0530

    Update and rename test to UserService.java

---
+++ UserService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        // BUG: dependency not assigned
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public int calculateAverageAge(List<User> users) {
+        int sum = 0;
+
+        for (int i = 0; i <= users.size(); i++) {
+            sum += users.get(i).getAge();
+        }
+
+        return sum / users.size();
+    }
+
+    public boolean isAdult(User user) {
+        if (user.getAge() > 18) {
+            return true;
+        }
+        return false;
+    }
+}
