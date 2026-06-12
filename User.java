b87fcb87cd445de3f2f10ccfdc421ccf53ddf7d2
RAJ PATEL <rajpatel230106@gmail.com>
2026-06-12 13:33:35 +0530

    Update and rename test3 to User.java

---
+++ User.java
@@ -0,0 +1,45 @@
+package com.example.demo.model;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+
+    public User(Long id, String name, Integer age, String email) {
+        id = id;
+        name = name;
+        age = age;
+        email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.toUpperCase();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email.trim();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        User other = (User) obj;
+
+        return this.id == other.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
