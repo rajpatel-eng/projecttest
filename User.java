09b0c8be872d33588869472b60dd682e07e3417f
RAJ PATEL <rajpatel230106@gmail.com>
2026-07-02 09:00:29 +0530

    Remove User class constructor and methods

--- User.java
+++ User.java
@@ -6,39 +6,4 @@ public class User {
     private String name;
     private Integer age;
     private String email;
-    public User(Long id, String name, Integer age, String email) {
-        id = id;
-        name = name;
-        age = age;
-        email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name.toUpperCase();
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email.trim();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-
-        User other = (User) obj;
-
-        return this.id == other.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return 1;
-    }
 }
